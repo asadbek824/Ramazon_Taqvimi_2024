9b5c09a4588e67480e43ace9ecadcb1ff9335614
Islombek Akmadjonov <nexiar280hp@gmail.com>
2024-02-14 23:43:21 +0500

    [duaPage] created DuaViewController

---
+++ Ramazon Taqvimi 2024/DuaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DuaViewController.swift
+//  Ramazon Taqvimi 2024
+//
+//  Created by islombek on 14/02/24.
+//
+
+import UIKit
+
+final class DuaViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
